f09ed0f3a9fee6ad4fd5c21942a35b21eb1e3f57
Essserrr <blizzz93@gmail.com>
2021-03-01 19:16:19 +0900

    review slider can be slided from map

--- src/public/js/review/review.js
+++ src/public/js/review/review.js
@@ -3,8 +3,44 @@ $(function () {
         console.log('Bx slider is not connected');
         return;
     }
-    $('.js-review-slider').bxSlider({
+
+    let prevTarget = $('.js-review-flag#flag-0');
+
+    const slideCallback = function ($slideElement, oldIndex, newIndex) {
+        const newID = `flag-${newIndex}`;
+        if (prevTarget.attr('id') === newID) return;
+
+        const target = $(`#${newID}`);
+
+        target.addClass('active');
+        if (prevTarget) {
+            prevTarget.removeClass('active');
+        }
+        prevTarget = target;
+    };
+
+    slider = $('.js-review-slider').bxSlider({
         pager: false,
         controls: true,
+        onSlideBefore: slideCallback,
     });
+
+    const flagClickHandler = function (event) {
+        const target = $(event.currentTarget);
+
+        if (prevTarget.attr('id') === target.attr('id')) return;
+
+        const reviewID = target.attr('id').slice(5);
+
+        slider.goToSlide(reviewID);
+
+        target.addClass('active');
+        if (prevTarget) {
+            prevTarget.removeClass('active');
+        }
+
+        prevTarget = target;
+    };
+
+    $('.js-review-flag').on('click', flagClickHandler);
 });
