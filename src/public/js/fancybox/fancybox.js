d051896b59568e00f49d1d38f86d911daf174214
Essserrr <blizzz93@gmail.com>
2021-03-01 13:03:17 +0900

    fancybox added

---
+++ src/public/js/fancybox/fancybox.js
@@ -0,0 +1,16 @@
+$(function () {
+    if (!$().lettering) {
+        console.log('Lettering is not connected');
+        return;
+    }
+
+    const phonecallbackHandler = function (event) {
+        const opens = $(event.currentTarget).attr('data-open-target');
+        $.fancybox.open({
+            src: opens,
+            type: 'inline',
+        });
+    };
+
+    $('.js-callback').on('click', phonecallbackHandler);
+});
