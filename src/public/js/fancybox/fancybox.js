e1106aa5b5b3899a0923aaa25e8d5dcb6a9adb54
Essserrr <blizzz93@gmail.com>
2021-03-03 12:00:22 +0900

    review diolog added

--- src/public/js/fancybox/fancybox.js
+++ src/public/js/fancybox/fancybox.js
@@ -1,10 +1,11 @@
 $(function () {
-    if (!$().lettering) {
-        console.log('Lettering is not connected');
+    if (!$.fancybox) {
+        console.log('fancybox is not connected');
         return;
     }
 
     const phonebackHandler = function (event) {
+        console.log($(event.currentTarget).attr('data-open-target'))
         const opens = $(event.currentTarget).attr('data-open-target');
         $.fancybox.open({
             src: opens,
@@ -14,6 +15,17 @@ $(function () {
 
     $('.js-phoneback').on('click', phonebackHandler);
     $('.js-costs').on('click', phonebackHandler);
+
+
+    const reviewHandler = function (event) {
+        const opens = $(event.currentTarget).parents(".review").find(".diolog");
+
+        $.fancybox.open({
+            src: opens,
+            type: 'inline',
+        });
+    };
+    $('.js-read-more').on('click', reviewHandler);
 
     const closeHandler = function () {
         $.fancybox.close();
